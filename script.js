63103dc014bca780f2b3c7f9e0e27f07b02847c5
Denex A Poulose <107526656+Denex8Poulose@users.noreply.github.com>
2022-07-15 11:38:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+ ordered=()=>{
+  document.getElementById('inform').innerHTML = 'your order was placed!';
+
+  document.getElementById('inform2').innerHTML = "Thanks for Shopping !Your Products hasn't shipped yet.we'll inform you when it was done."
+
+}
